394d647141437a9deb3c31c34daf59c6e8510cf0
Evelina Kunsmanaite <evelinakunsmanaite@gmail.com>
2023-06-07 18:40:35 +0300

    get time

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        System.out.println("Сейчас: " + localDateTime.format(formatter));
+        System.out.println("");
+
         SystemInformation.getSystemInformation();
+
         System.out.println("Конечная стоимость топлива " + FillingStation.countPrice());
     }
 }

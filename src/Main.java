f0b87513ed510f1d7cca13a00bda473f002fd647
Evelina Kunsmanaite <evelinakunsmanaite@gmail.com>
2023-06-07 18:26:39 +0300

    get information about system

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        SystemInformation.getSystemInformation();
         System.out.println("Конечная стоимость топлива " + FillingStation.countPrice());
     }
 }
--- src/SystemInformation.java
+++ src/SystemInformation.java
@@ -1,2 +1,13 @@
 public class SystemInformation {
+
+    public static void getSystemInformation(){
+        System.out.println("Информация о системе: ");
+        System.out.print("ОС: ");
+        System.out.println(System.getProperty("os.name") + " " + System.getProperty("os.version"));
+        System.out.print("Количество ядер процессора: ");
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.print("Количество памяти, доступной JVM: ");
+        System.out.println(Runtime.getRuntime().maxMemory() / 1024 / 1024 + "Mb");
+        System.out.println("");
+    }
 }

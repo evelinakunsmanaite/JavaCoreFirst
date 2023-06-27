5e03e232ebba8742cbfe49bda3628dd83599ee30
Evelina Kunsmanaite <evelinakunsmanaite@gmail.com>
2023-06-27 17:45:37 +0300

    Uncommented

--- src/Main.java
+++ src/Main.java
@@ -6,55 +6,55 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
-//        LocalDateTime localDateTime = LocalDateTime.now();
-//
-//        System.out.println("Сейчас: " + localDateTime.format(formatter) + "\n");
-//
-//        SystemInformation.getSystemInformation();
-//
-//        System.out.println("Конечная стоимость топлива " + FillingStation.countPrice() + "\n");
-//
-//        Tasks.firstTask();
-//
-//        Tasks.secondTask();
-//
-//        Tasks.thirdTask();
-//
-//        System.out.println("\nВведите число от 0 до 9");
-//        int number = scanner.nextInt();
-//        Tasks.fourthTask(number);
-//
-//        System.out.println("\nВведите номер этажа");
-//        int floorNumber = scanner.nextInt();
-//        Tasks.fifthTask(floorNumber);
-//
-//        System.out.println("\nВведите диапазон от а до b");
-//        System.out.println("\nВведите а");
-//        int a = scanner.nextInt();
-//        System.out.println("\nВведите b");
-//        int b = scanner.nextInt();
-//
-//        if (a >= b) {
-//            System.out.println("Данные введены некорректно");
-//        } else {
-//            Tasks.sixthTask(a, b);
-//        }
-//
-//        System.out.println("\nВведите x");
-//        double x = scanner.nextDouble();
-//        Tasks.seventhTask(x);
-//
-//        while (true) {
-//            System.out.println("\nВведите страну");
-//            String cityName = scanner.next();
-//            if (!cityName.equals("stop")) {
-//                Tasks.getCapitalByCity(cityName);
-//            } else {
-//                System.out.println("Завершение программы");
-//                break;
-//            }
-//        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        System.out.println("Сейчас: " + localDateTime.format(formatter) + "\n");
+
+        SystemInformation.getSystemInformation();
+
+        System.out.println("Конечная стоимость топлива " + FillingStation.countPrice() + "\n");
+
+        Tasks.firstTask();
+
+        Tasks.secondTask();
+
+        Tasks.thirdTask();
+
+        System.out.println("\nВведите число от 0 до 9");
+        int number = scanner.nextInt();
+        Tasks.fourthTask(number);
+
+        System.out.println("\nВведите номер этажа");
+        int floorNumber = scanner.nextInt();
+        Tasks.fifthTask(floorNumber);
+
+        System.out.println("\nВведите диапазон от а до b");
+        System.out.println("\nВведите а");
+        int a = scanner.nextInt();
+        System.out.println("\nВведите b");
+        int b = scanner.nextInt();
+
+        if (a >= b) {
+            System.out.println("Данные введены некорректно");
+        } else {
+            Tasks.sixthTask(a, b);
+        }
+
+        System.out.println("\nВведите x");
+        double x = scanner.nextDouble();
+        Tasks.seventhTask(x);
+
+        while (true) {
+            System.out.println("\nВведите страну");
+            String cityName = scanner.next();
+            if (!cityName.equals("stop")) {
+                Tasks.getCapitalByCity(cityName);
+            } else {
+                System.out.println("Завершение программы");
+                break;
+            }
+        }
 
         while (true) {
             System.out.println("\nВведите делимое");

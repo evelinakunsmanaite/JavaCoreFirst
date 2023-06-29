dda49b43b45f5babac9ffd05d19ad548d3f25264
Evelina Kunsmanaite <evelinakunsmanaite@gmail.com>
2023-06-29 17:34:05 +0300

    create new class

---
+++ src/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private int year;
+    private String color, model;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "\nyear: " + year +
+                "\ncolor: " + color +
+                "\nmodel: " + model;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,91 +6,109 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
-        LocalDateTime localDateTime = LocalDateTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
+//        LocalDateTime localDateTime = LocalDateTime.now();
+//
+//        System.out.println("Сейчас: " + localDateTime.format(formatter) + "\n");
+//
+//        SystemInformation.getSystemInformation();
+//
+//        System.out.println("Конечная стоимость топлива " + FillingStation.countPrice() + "\n");
+//
+//        Tasks.firstTask();
+//
+//        Tasks.secondTask();
+//
+//        Tasks.thirdTask();
+//
+//        System.out.println("\nВведите число от 0 до 9");
+//        int number = scanner.nextInt();
+//        Tasks.fourthTask(number);
+//
+//        System.out.println("\nВведите номер этажа");
+//        int floorNumber = scanner.nextInt();
+//        Tasks.fifthTask(floorNumber);
+//
+//        System.out.println("\nВведите диапазон от а до b");
+//        System.out.println("\nВведите а");
+//        int a = scanner.nextInt();
+//        System.out.println("\nВведите b");
+//        int b = scanner.nextInt();
+//
+//        if (a >= b) {
+//            System.out.println("Данные введены некорректно");
+//        } else {
+//            Tasks.sixthTask(a, b);
+//        }
+//
+//        System.out.println("\nВведите x");
+//        double x = scanner.nextDouble();
+//        Tasks.seventhTask(x);
+//
+//        while (true) {
+//            System.out.println("\nВведите страну");
+//            String cityName = scanner.next();
+//            if (!cityName.equals("stop")) {
+//                Tasks.getCapitalByCity(cityName);
+//            } else {
+//                System.out.println("Завершение программы");
+//                break;
+//            }
+//        }
+//
+//        while (true) {
+//            System.out.println("\nВведите делимое");
+//            double dividend = scanner.nextDouble();
+//            System.out.println("\nВведите делитель");
+//            double divisor = scanner.nextDouble();
+//            if (divisor != 0) {
+//                Tasks.dividedBy(dividend, divisor);
+//            } else {
+//                System.out.println("Завершение программы");
+//                break;
+//            }
+//        }
+//
+//        int i = 0;
+//        while (i != 5) {
+//            System.out.println("\nВведите делимое");
+//            double dividend = scanner.nextDouble();
+//            System.out.println("\nВведите делитель");
+//            double divisor = scanner.nextDouble();
+//            if (divisor == 0) {
+//                System.out.println("Деление на 0");
+//            } else {
+//                Tasks.dividedBy(dividend, divisor);
+//            }
+//            i++;
+//        }
+//
+//        System.out.println("Введите размер массива: ");
+//        int arrayLength = scanner.nextInt();
+//        SecondTask.createArrayAndGetInfo(arrayLength);
+//
+//        SecondTask.getArrayInfo();
+//
+//        SecondTask.multiDimensionalArrays();
+//
+//        SecondTask.getMaxSum();
 
-        System.out.println("Сейчас: " + localDateTime.format(formatter) + "\n");
+        Car car = new Car();
+        car.setYear(2017);
+        car.setColor("black");
+        car.setModel("Porsche Panamera E-Hybrid");
 
-        SystemInformation.getSystemInformation();
+        System.out.println("Год выпуска: " + car.getYear());
+        System.out.println("Цвет: " + car.getColor());
+        System.out.println("Модель: " + car.getModel());
 
-        System.out.println("Конечная стоимость топлива " + FillingStation.countPrice() + "\n");
+        Car secondCar = new Car();
+        secondCar.setYear(2019);
+        secondCar.setColor("white");
+        secondCar.setModel("Ford Focus");
 
-        Tasks.firstTask();
+        System.out.println(secondCar);
 
-        Tasks.secondTask();
 
-        Tasks.thirdTask();
-
-        System.out.println("\nВведите число от 0 до 9");
-        int number = scanner.nextInt();
-        Tasks.fourthTask(number);
-
-        System.out.println("\nВведите номер этажа");
-        int floorNumber = scanner.nextInt();
-        Tasks.fifthTask(floorNumber);
-
-        System.out.println("\nВведите диапазон от а до b");
-        System.out.println("\nВведите а");
-        int a = scanner.nextInt();
-        System.out.println("\nВведите b");
-        int b = scanner.nextInt();
-
-        if (a >= b) {
-            System.out.println("Данные введены некорректно");
-        } else {
-            Tasks.sixthTask(a, b);
-        }
-
-        System.out.println("\nВведите x");
-        double x = scanner.nextDouble();
-        Tasks.seventhTask(x);
-
-        while (true) {
-            System.out.println("\nВведите страну");
-            String cityName = scanner.next();
-            if (!cityName.equals("stop")) {
-                Tasks.getCapitalByCity(cityName);
-            } else {
-                System.out.println("Завершение программы");
-                break;
-            }
-        }
-
-        while (true) {
-            System.out.println("\nВведите делимое");
-            double dividend = scanner.nextDouble();
-            System.out.println("\nВведите делитель");
-            double divisor = scanner.nextDouble();
-            if (divisor != 0) {
-                Tasks.dividedBy(dividend, divisor);
-            } else {
-                System.out.println("Завершение программы");
-                break;
-            }
-        }
-
-        int i = 0;
-        while (i != 5) {
-            System.out.println("\nВведите делимое");
-            double dividend = scanner.nextDouble();
-            System.out.println("\nВведите делитель");
-            double divisor = scanner.nextDouble();
-            if (divisor == 0) {
-                System.out.println("Деление на 0");
-            } else {
-                Tasks.dividedBy(dividend, divisor);
-            }
-            i++;
-        }
-
-        System.out.println("Введите размер массива: ");
-        int arrayLength = scanner.nextInt();
-        SecondTask.createArrayAndGetInfo(arrayLength);
-
-        SecondTask.getArrayInfo();
-
-        SecondTask.multiDimensionalArrays();
-
-        SecondTask.getMaxSum();
     }
 }
